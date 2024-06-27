72ff9d77863564ef1d0aadc200e69307eebf2b68
Kalvin Calimag <calimag.kalvin.d@gmail.com>
2024-06-28 00:28:21 +0800

    Implemented custom Admin Panel and further configurations

--- kcoin/settings.py
+++ kcoin/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -132,3 +133,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+JAZZMIN_SETTINGS ={
+    "site_title": "KCoin",
+    "site_header": "KCoin",
+    "site_brand": "Payment Made Easy...",
+    # "site_logo": "path/logo.png",
+    "copyright": "- KCoin Company Ltd.",
+    # "order_with_respect_to": ["core", "user_auths", "transactions", "addons", "blog"]
+}
