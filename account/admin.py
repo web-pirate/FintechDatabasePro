d61f48ccc806de37a6d62c8040a335f2924d80c7
Kalvin Calimag <calimag.kalvin.d@gmail.com>
2024-07-04 12:56:03 +0800

    Created account app, model, and registered to admin.py

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Account
+from user_auths.models import CustomUser
+from import_export.admin import ImportExportModelAdmin
 
-# Register your models here.
+class AccountAdminModel(ImportExportModelAdmin):
+    list_editable = ["account_status", "account_balance"]
+    list_display = ["user", "account_number", "account_status", "account_balance"]
+    list_filter = ["account_status"]
+
+# class KYCAdmin(ImportExportModelAdmin):
+#     model = Account
+#     search_fields = ["full_name"]
+#     list_display = ["user", "full_name"]
+
+admin.site.register(Account, AccountAdminModel)
