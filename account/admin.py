ed058604a273bbbbed6763c702d18f1d103f666b
Kalvin Calimag <calimag.kalvin.d@gmail.com>
2024-07-06 20:39:56 +0800

    Added KYC model for customer identity verification

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Account
+from .models import Account, KYC
 from user_auths.models import CustomUser
 from import_export.admin import ImportExportModelAdmin
 
@@ -8,9 +8,9 @@ class AccountAdminModel(ImportExportModelAdmin):
     list_display = ["user", "account_number", "account_status", "account_balance"]
     list_filter = ["account_status"]
 
-# class KYCAdmin(ImportExportModelAdmin):
-#     model = Account
-#     search_fields = ["full_name"]
-#     list_display = ["user", "full_name"]
+class KYCAdmin(ImportExportModelAdmin):
+    search_fields = ["full_name"]
+    list_display = ["user", "full_name"]
 
 admin.site.register(Account, AccountAdminModel)
+admin.site.register(KYC, KYCAdmin)
--- account/models.py
+++ account/models.py
@@ -51,16 +51,35 @@ class Account(models.Model):
     
     class Meta: 
         ordering = ['-date']
-        
-    # def __str__(self):
-    #     try: 
-    #         return self.user
-    #     except: 
-    #         return "Account Model"
     
     def __str__(self):
         return f"{self.user}"
+    
+class KYC(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=1000)
+    image = models.ImageField(upload_to="kyc", default="default.jpg")
+    nationality = models.CharField(max_length=100)
+    marital_status = models.CharField(choices=MARITAL_STATUS, max_length=40)
+    gender = models.CharField(choices=GENDER, max_length=40)    
+    identity_type = models.CharField(choices=IDENTITY_TYPE, max_length=140)
+    date_of_birth = models.DateTimeField(auto_now_add=False)
+    signature = models.ImageField(upload_to="kyc")
+    
+    # Address
+    country = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
 
+    # Contact Detail
+    mobile = models.CharField(max_length=1000)
+    fax = models.CharField(max_length=1000)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.user}"
+    
 def create_account(sender, instance, created, **kwargs):
     if created: 
         Account.objects.create(user=instance)
@@ -70,3 +89,5 @@ def save_account(sender, instance, **kwargs):
     
 post_save.connect(create_account, sender=CustomUser)
 post_save.connect(save_account, sender=CustomUser)
+
+
