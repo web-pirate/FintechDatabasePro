131fc520976b6e890348a708ab47cea0686e4b68
Kalvin Calimag <calimag.kalvin.d@gmail.com>
2024-07-09 09:26:16 +0800

    Refine KYC forms, views, URLs, and templates for enhanced functionality

--- account/views.py
+++ account/views.py
@@ -26,5 +26,6 @@ def kyc_registration(request):
     context = {
         "account": account,
         "form": form,
+        "kyc": kyc,
     }
     return render(request, "account/kyc-form.html", context)
--- kcoin/urls.py
+++ kcoin/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from core import views
 
@@ -25,3 +27,9 @@ urlpatterns = [
     path("user/", include("user_auths.urls")),
     path("account/", include("account.urls")),
 ]
+
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
